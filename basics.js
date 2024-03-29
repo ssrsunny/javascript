7f8f7ac001af1d0506402812b55c5d574f82ce83
ssrsunny <sunnyssr95@gmail.com>
2024-03-29 11:29:02 +0530

    added basics

---
+++ basics.js
@@ -0,0 +1,56 @@
+// variables
+let variab = 'sunny'
+console.log(variab);
+
+//constant
+const interest = 10
+console.log(interest);
+
+/*
+primitive types
+String, Number, Boolean, undefined, null
+
+reference types
+objects, arrays, functions
+*/
+
+//objects
+let person = {
+    name: 'suny',
+    age: 40,
+    height: 1.90
+}
+
+person.name = 'sunnny'
+console.log(person.name)
+console.log(person['height'])
+
+let selection = "age"
+person[selection]
+console.log(person[selection]);
+
+// here the age of the person is modified
+person[selection] = 45
+console.log(person[selection]);
+
+console.log(person);
+console.log(typeof person);
+
+// arrays
+let colors = ['red', 'green', 'blue']
+console.log(colors);
+console.log(typeof colors);
+
+// functions
+function greet(){
+    console.log('Hello world')
+}
+greet()
+
+function greetPerson(fname, lname) {
+    console.log('Hello ' + fname + ' ' + lname);
+}
+greetPerson('sunny','rithvik')
+greetPerson('sunny')
+greetPerson()
+
---
+++ operators.js
@@ -0,0 +1,30 @@
+/* 
+arithmetic
++ - * / % ** ++ --
+
+assignment
+=  +=  -=  *=  /=  %=
+
+comparison
+== equal to
+=== equal value and equal type
+!=  !==  >  <  >=  <=
+
+ternary
+let variable_name = condition ? value1 : value2
+
+logical
+&&(and), ||(or), !(not)
+
+truthy operand
+=> short circuiting when using ||
+
+Falsy values
+false, undefined, null, 0, ''(empty string), NaN(not a number)
+
+bitwise
+&(AND), |(OR), ~(NOT), ^(XOR), <<(LEFT SHIFT), >>(RIGHT SHIFT)
+| -> Add
+& -> Check
+
+*/
