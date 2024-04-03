0f6b7cee81d2b72c9540313c2d8a0bd7bd025ce6
ssrsunny <sunnyssr95@gmail.com>
2024-04-03 13:00:54 +0530

    modified

--- Arrays/adding elements.js
+++ Arrays/adding elements.js
@@ -27,8 +27,7 @@ console.log(number.lastIndexOf(1));
 
 
 // To verify if a given elements exist in an array
-console.log(number.indexOf(1) !== -1); // This expression will return true or false. Depending if the element exist in the array.
+console.log(number.indexOf(1) !== -1);
 
-// This a new better method to achieve the same thing.
 console.log(number.includes(1));
 console.log(number.includes('a'));
