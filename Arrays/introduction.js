67860ae0cf9d4165f59f77c9706fae7be285e0cc
ssrsunny <sunnyssr95@gmail.com>
2024-04-03 11:52:30 +0530

    added introduction

---
+++ Arrays/introduction.js
@@ -0,0 +1,12 @@
+
+// Initializing an array with the Constructor
+let myArray1 = new Array();
+console.log(myArray1);
+
+// Creating an array literal 
+let myArray2 = [];
+console.log(myArray2);
+
+// Using "const"
+const myArray3 = [];
+console.log(myArray3);
